26de45846f13fef12745e0dc9353bf625ba576ea
buihang <camhang190497@gmail.com>
2022-01-08 20:41:31 +0700

    add search form

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Card, CardTitle, CardImg, Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardTitle, CardImg, Button, Form, FormGroup, Input} from 'reactstrap';
 import { Link } from 'react-router-dom';
 
     function RenderStaffItem({staff, onClick}) {
@@ -15,39 +15,86 @@ import { Link } from 'react-router-dom';
         );
     }  
 
-    const StaffList = (props) => {
+    class StaffList extends Component {
+        constructor(props) {
+            super(props);
 
-        const stafflist = props.staffs.map((staff) => {
-            return(
-                <div key={staff.id} className="col-6 col-sm-4 col-md-2">
-                    <RenderStaffItem staff={staff} />
-                </div>
-            )
-        })
-        return(
-            <div className="container">
-                <div className="row">
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to='/trangchu'>Trang chủ</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>Nhân  viên</BreadcrumbItem>
-                    </Breadcrumb>
-                    <div  className="col-12">
-                        <h3>Nhân  viên</h3>
-                        <hr/>
+            this.state= {
+                staffName: '',
+                id: '',
+                id: 0,
+                name: '',
+                doB: '',
+                salaryScale: '',
+                startDate: '',
+                department: '',
+                annualLeave:'' ,
+                overTime: '',
+                image: '/assets/images/alberto.png',
+
+            }
+            this.handleSearch = this.handleSearch.bind(this);
+        }
+
+    
+
+        handleSearch(event) {
+            const key = event.target.key.value;
+           //alert(this.input.value)
+           
+            
+            this.setState({ staffName: key})
+            event.preventDefault();
+        }
+    
+        render() {
+
+            const stafflist = this.props.staffs
+            .filter((staff) => {
+                if (this.state.staffName === '') return staff;
+                else if (staff.name.toLowerCase().includes(this.state.staffName.toLowerCase())) return staff;
+                return 0;
+            })
+            .map((staff) => {
+                return(
+                    <div key={staff.id} className="col-6 col-sm-4 col-md-2">
+                        <RenderStaffItem staff={staff} />
                     </div>
+                )
+            });
+
+            return(
+                <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <nav className="navbar navbar-light bg-light justify-content-between">
+                            <a className="navbar-brand">Nhân Viên</a>
+                            <Button><i className="fa fa-plus fa-lg"></i></Button>
+                            <Form className="form-inline"
+                                onSubmit={this.handleSearch}
+                            > 
+                                <Input type="text" 
+                                       id="searchkey" 
+                                       name="key"
+                                       className="form-control mr-sm-2" 
+                                />
+                                <Button className="btn btn-outline-success my-2 my-sm-0" 
+                                        type="submit" 
+                                        value="Submit"
+                                >Search
+                                </Button>
+                            </Form>
+                        </nav>
+                    </div>
+                   
                 </div>
                 <div className="row">
                     {stafflist}
                 </div>
                 
             </div>
-
-        )
+            );
+        }
     }
-
-        
-    
-
-
 
 export default StaffList;
